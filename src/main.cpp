2ceab0b0434de82f9c1df0074c07049ca3e8de3b
Saritahhh <tusmuertos018@gmail.com>
2025-06-15 18:20:22 -0600

    HI CHAT

---
+++ src/main.cpp
@@ -0,0 +1,76 @@
+#include <Geode/modify/PlayerObject.hpp>
+#include <Geode/modify/PlayLayer.hpp>
+
+using namespace geode::prelude;
+
+class $modify(FlappyPlayerObject, PlayerObject) {
+	struct Fields{
+		bool isDownRotated = false;
+	};
+
+	void cooldownRot(float dt){
+		m_fields->isDownRotated = true;
+	}
+
+	void update(float p0) {
+		PlayerObject::update(p0);
+
+		if (!m_isBird) return;
+
+		if (m_fields->isDownRotated && PlayLayer::get()) {
+			this->setRotation(60.f);
+		}
+
+	}
+
+	void hitGround(GameObject* p0, bool p1) {
+		PlayerObject::hitGround(p0, p1);
+
+		if (!m_isBird) return;
+
+		if(PlayLayer::get()) {
+			this->stopAllActions();
+			this->unscheduleAllSelectors();
+			m_fields->isDownRotated = false;
+		}
+		
+	}
+
+	bool pushButton(PlayerButton p0) {
+		if (!PlayerObject::pushButton(p0)) return false;
+
+		auto fields = m_fields.self();
+		if (!m_isBird) return true;
+
+		if (PlayLayer::get()) {
+			this->stopAllActions();
+			fields->isDownRotated = false;
+			this->unscheduleAllSelectors();
+
+			auto justJumpedAnim = CCEaseOut::create(CCRotateTo::create(0.1f, -40.f), 2.0f);
+			auto theRotatingDownAnimThingLol = CCEaseIn::create(CCRotateTo::create(0.6f, 60.f), 2.0f);
+
+			auto bothLol = CCSequence::create(
+				justJumpedAnim,
+				theRotatingDownAnimThingLol,
+				nullptr
+			);
+
+			this->runAction(bothLol);
+			this->scheduleOnce(schedule_selector(FlappyPlayerObject::cooldownRot), 0.7f);
+		}
+		return true;
+
+	}
+};
+
+class $modify(FlappyPlayLayer, PlayLayer) {
+	void resetLevel() {
+		PlayLayer::resetLevel();
+
+		m_player1->stopAllActions();
+		m_player1->unscheduleAllSelectors();
+		m_player2->stopAllActions();
+		m_player2->unscheduleAllSelectors();
+	}
+};
